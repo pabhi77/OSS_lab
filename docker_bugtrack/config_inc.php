5724d9173507e636ed787c113c4dae4b5d94fb58
pabhi77 <86058581+pabhi77@users.noreply.github.com>
2023-12-14 13:17:38 +0530

    Add files via upload

---
+++ docker_bugtrack/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$g_hostname = 'mariadb'; // Use the hostname of your MySQL/MariaDB container
+$g_db_type = 'mysqli';
+$g_database_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = 'root';
+
+$g_crypto_master_salt = 'random_salt';
+
